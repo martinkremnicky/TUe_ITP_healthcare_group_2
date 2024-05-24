1138e729c039ebd76cfed8d9b55eec84ad7e34a8
Hilde Kerkhoven <hilde.kerkhoven@ru.nl>
2024-05-24 11:07:18 +0200

    added comments for readability

--- data_characteristics/data_statistics.py
+++ data_characteristics/data_statistics.py
@@ -5,10 +5,12 @@ data = pd.read_csv("C:/Users/hilde/Documents/Technische Universiteit Eindhoven/I
 
 # calculate the number of patients
 number_patients = len(data)
-print(number_patients)
+print("number of patients is: ", number_patients)
 
 # the number of patients with a malignent tumor (0 is benign, 1 is malignent)
 malignent_patients= data.loc[data['malignant/benign'] == 1.0]
-print(len(malignent_patients))
+print("number of malignent patients is: ", len(malignent_patients))
 
 # the average age of the patients
+average_age = data.loc[:,'age'].mean()
+print("the average age is: ", average_age)
